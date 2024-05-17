c341312dad3b22dcc191d58e994491a0a9bc76fe
DarkPython69 <DarkPython69>
2024-05-17 16:06:26 +0000

    Fixed some errors in the git ignore

--- build/lib/blockmango/__init__.py
+++
@@ -1,8 +0,0 @@
-# Inside blockmango/__init__.py
-
-from .clan import Clan
-from .user import User
-from .groupchat import Group
-from .friends import Friends
-from .decoration import Decoration
-from .http import HTTPMixin
